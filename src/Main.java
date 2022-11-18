35d9da821f7b0a538b3fd589ae9364bcf0f87a23
Ivan Ladan <kalanree72@gmail.com>
2022-11-19 00:02:57 +1100

    first commit

---
+++ src/Main.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<String> names = Arrays.asList("Jack", "Connor", "Harry", "George", "Samuel", "John");
+        List<String> families = Arrays.asList("Evans", "Young", "Harris", "Wilson", "Davies", "Adamson", "Brown");
+        Collection<Person> persons = new ArrayList<>();
+        for (int i = 0; i < 10_000_000; i++) {
+            persons.add(new Person(
+                    names.get(new Random().nextInt(names.size())),
+                    families.get(new Random().nextInt(families.size())),
+                    new Random().nextInt(100),
+                    Sex.values()[new Random().nextInt(Sex.values().length)],
+                    Education.values()[new Random().nextInt(Education.values().length)])
+            );
+//        Найти количество несовершеннолетних (т.е. людей младше 18 лет).
+
+            long personYoung = persons.stream()
+                    .filter(x -> x.getAge() < 18)
+                    .count();
+            System.out.println(personYoung);
+
+
+
+
+
+
+//        Получить список фамилий призывников (т.е. мужчин от 18 и до 27 лет).
+
+
+
+
+
+//        Получить отсортированный по фамилии список потенциально работоспособных людей с высшим образованием в выборке (т.е. людей с высшим образованием от 18 до 60 лет для женщин и до 65 лет для мужчин).
+
+        }
+    }
+}
